d31d439ae7cbef2fcaca53c9848569069c07b388
Paulius Šukys <paulius.sukys@ktu.lt>
2014-05-09 23:11:30 +0300

    Susitvarkiau Android paskyrą, pridėjau pradinį projektą sukonfigūruotą XCover2'am. Ateityje paketas mobile bus išimtas ir pilnai perkeltas į android

---
+++ SmartFoodMobile/bin/classes.dex.d
@@ -0,0 +1,8 @@
+/home/shookees/Documents/KTU/Bakalauras 2011-2015/3 kursas/2 pusmetis/[T120B173] Daugiaagenčių sistemų pagrindai/kursinis/SmartFoodMobile/bin/classes.dex : \
+/home/shookees/Documents/KTU/Bakalauras 2011-2015/3 kursas/2 pusmetis/[T120B173] Daugiaagenčių sistemų pagrindai/kursinis/SmartFoodMobile/bin/classes/ktu/smartfood/R.class \
+/home/shookees/Documents/KTU/Bakalauras 2011-2015/3 kursas/2 pusmetis/[T120B173] Daugiaagenčių sistemų pagrindai/kursinis/SmartFoodMobile/bin/classes/ktu/smartfood/R$attr.class \
+/home/shookees/Documents/KTU/Bakalauras 2011-2015/3 kursas/2 pusmetis/[T120B173] Daugiaagenčių sistemų pagrindai/kursinis/SmartFoodMobile/bin/classes/ktu/smartfood/BuildConfig.class \
+/home/shookees/Documents/KTU/Bakalauras 2011-2015/3 kursas/2 pusmetis/[T120B173] Daugiaagenčių sistemų pagrindai/kursinis/SmartFoodMobile/bin/classes/ktu/smartfood/R$layout.class \
+/home/shookees/Documents/KTU/Bakalauras 2011-2015/3 kursas/2 pusmetis/[T120B173] Daugiaagenčių sistemų pagrindai/kursinis/SmartFoodMobile/bin/classes/ktu/smartfood/R$drawable.class \
+/home/shookees/Documents/KTU/Bakalauras 2011-2015/3 kursas/2 pusmetis/[T120B173] Daugiaagenčių sistemų pagrindai/kursinis/SmartFoodMobile/bin/classes/ktu/smartfood/MainActivity.class \
+/home/shookees/Documents/KTU/Bakalauras 2011-2015/3 kursas/2 pusmetis/[T120B173] Daugiaagenčių sistemų pagrindai/kursinis/SmartFoodMobile/bin/classes/ktu/smartfood/R$string.class \
---
+++ SmartFoodMobile/gen/R.java.d
@@ -0,0 +1,8 @@
+/home/shookees/Documents/KTU/Bakalauras 2011-2015/3 kursas/2 pusmetis/[T120B173] Daugiaagenčių sistemų pagrindai/kursinis/SmartFoodMobile/gen/ktu/smartfood/R.java \
+ : /home/shookees/Documents/KTU/Bakalauras 2011-2015/3 kursas/2 pusmetis/[T120B173] Daugiaagenčių sistemų pagrindai/kursinis/SmartFoodMobile/res/drawable-hdpi/ic_launcher.png \
+/home/shookees/Documents/KTU/Bakalauras 2011-2015/3 kursas/2 pusmetis/[T120B173] Daugiaagenčių sistemų pagrindai/kursinis/SmartFoodMobile/res/layout/main.xml \
+/home/shookees/Documents/KTU/Bakalauras 2011-2015/3 kursas/2 pusmetis/[T120B173] Daugiaagenčių sistemų pagrindai/kursinis/SmartFoodMobile/res/drawable-xhdpi/ic_launcher.png \
+/home/shookees/Documents/KTU/Bakalauras 2011-2015/3 kursas/2 pusmetis/[T120B173] Daugiaagenčių sistemų pagrindai/kursinis/SmartFoodMobile/res/drawable-mdpi/ic_launcher.png \
+/home/shookees/Documents/KTU/Bakalauras 2011-2015/3 kursas/2 pusmetis/[T120B173] Daugiaagenčių sistemų pagrindai/kursinis/SmartFoodMobile/res/values/strings.xml \
+/home/shookees/Documents/KTU/Bakalauras 2011-2015/3 kursas/2 pusmetis/[T120B173] Daugiaagenčių sistemų pagrindai/kursinis/SmartFoodMobile/res/drawable-ldpi/ic_launcher.png \
+/home/shookees/Documents/KTU/Bakalauras 2011-2015/3 kursas/2 pusmetis/[T120B173] Daugiaagenčių sistemų pagrindai/kursinis/SmartFoodMobile/bin/AndroidManifest.xml \
