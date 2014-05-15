fb29c0135ee5338c7a665349aa4dfc7f33009036
Paulius Šukys <paulius.sukys@ktu.lt>
2014-05-15 13:01:12 +0300

    Pridėjau teksto paiešką per lentelę (duomenys bus traukiami iš intelektualizuoto agento) Pridėjau mongodb biblioteką. TODO: sukurti mongodb

--- src/smartfood/mobile/GUI.java
+++ src/smartfood/mobile/GUI.java
@@ -33,13 +33,25 @@ import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.PatternSyntaxException;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.RowFilter;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableRowSorter;
 
 /**
  *
@@ -52,8 +64,12 @@ public class GUI extends JFrame
     private JComponent addPanel;
     private JComponent remPanel;
     private JComponent viePanel;
+    private JTable table;
+    private JTextField text;
+    private TableRowSorter<SFTableModel> sorter;
     private final Cam c = new Cam();
     private final Reader r = new Reader();
+    
     public GUI()
     {
         setName("SmartFood");
@@ -109,6 +125,87 @@ public class GUI extends JFrame
             
         });
         addPanel.add(readBarcode_button);
+        
+        //input box and result table
+        JButton inputProduct_button = new JButton("Input product");
+        inputProduct_button.addActionListener(new ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(ActionEvent ae)
+            {
+                Thread t = new Thread()
+                {
+                    public void run()
+                    {
+                        SFTableModel model = new SFTableModel();
+                        sorter = new TableRowSorter<SFTableModel>(model);
+                        table = new JTable(model);
+                        table.setRowSorter(sorter);
+                        table.setFillsViewportHeight(true);
+                        
+                        //single selector
+                        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+                        
+                        //when selection changes provide user with row numbers  for both view and model
+                        table.getSelectionModel().addListSelectionListener(
+                            new ListSelectionListener()
+                            {
+
+                                @Override
+                                public void valueChanged(ListSelectionEvent lse)
+                                {
+                                    int viewRow = table.getSelectedRow();
+                                    if (viewRow < 0)
+                                    {
+                                        Logger.getLogger(GUI.class.getName()).info("viewRow < 0");
+                                    }else
+                                    {
+                                        int modelRow = 
+                                          table.convertRowIndexToModel(viewRow);
+                                        Logger.getLogger(GUI.class.getName()).info("Selected row in view: " + viewRow + "; Selected row in model: " + modelRow);
+                                    }
+                                }
+                            });
+                        
+                        JScrollPane scrollPane = new JScrollPane(table);
+                        
+                        
+                        text = new JTextField();
+                        text.setPreferredSize(new Dimension(80, 20));
+                        text.getDocument().addDocumentListener(
+                        new DocumentListener()
+                        {
+
+                            @Override
+                            public void insertUpdate(DocumentEvent de)
+                            {
+                                filterData();
+                            }
+
+                            @Override
+                            public void removeUpdate(DocumentEvent de)
+                            {
+                                filterData();
+                            }
+
+                            @Override
+                            public void changedUpdate(DocumentEvent de)
+                            {
+                                filterData();
+                            }
+                        });
+                        addPanel.add(text);
+                        addPanel.add(scrollPane);
+                        pack();
+                    }
+                };
+                t.start();
+            }
+            
+        });
+        addPanel.add(inputProduct_button);
+        
         tabbedPane.add("Add", addPanel);
         tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
         
@@ -125,6 +222,19 @@ public class GUI extends JFrame
         tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
     }
     
+    private void filterData()
+    {
+        RowFilter<SFTableModel, Object> rf = null;
+        try
+        {
+            rf = RowFilter.regexFilter(text.getText(), 0);
+        }catch(PatternSyntaxException e)
+        {
+            return;
+        }
+        sorter.setRowFilter(rf);
+    }
+    
     /**
      * Creates a panel with label
      * @return panel
@@ -133,5 +243,38 @@ public class GUI extends JFrame
     {
         JPanel panel = new JPanel(false);
         return panel;
+    }
+}
+
+class SFTableModel extends AbstractTableModel
+{
+    private String[] columNames = {"Product name"};
+
+    private String[] data = {"Vardas", "Pavarde", "Dar", "Du zodziai", "lietuvių"};
+    @Override
+    public int getRowCount()
+    {
+        return data.length;
+    }
+
+    @Override
+    public int getColumnCount()
+    {
+        return columNames.length;
+    }
+    
+    public String getColumnName(int col) 
+    {
+        return columNames[col];
+    }
+    @Override
+    public Object getValueAt(int i, int i1)
+    {
+        return data[i];
+    }
+    
+    public Class getColumnClass(int c)
+    {
+        return getValueAt(0, c).getClass();
     }
 }
